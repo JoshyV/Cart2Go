4e71cd9442cc536af42ecab0de18c6ee48cc8ac8
JoshyV <josherinbaldespinosa@gmail.com>
2024-06-18 15:34:47 +0800

    ADd Inventory 40

---
+++ app/src/main/java/com/joshy/cart2go/Add_Inventory.java
@@ -0,0 +1,14 @@
+package com.joshy.cart2go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Inventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_inventory);
+    }
+}
